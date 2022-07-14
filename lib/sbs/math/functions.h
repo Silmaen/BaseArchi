c9b901b46e3bb674d491f3f1c6acbf075bcb1ea9
Silmaen <genteur.slayer@laposte.net>
2022-07-14 18:11:54 +0200

    Adding VEML6075 and first MKR Env version

--- lib/sbs/math/functions.h
+++ lib/sbs/math/functions.h
@@ -1,6 +1,6 @@
 /**
  * @file functions.h
- * @author argawaen
+ * @author Silmaen
  * @date 11/07/2022
  * Copyright © 2022 All rights reserved.
  * All modification must get authorization from the author.
--- test/test_sensor/lps22hb_utest.cpp
+++ test/test_sensor/lps22hb_utest.cpp
@@ -1,6 +1,6 @@
 /**
  * @file bme280_utest.cpp
- * @author argawaen
+ * @author Silmaen
  * @date 08/07/2022
  * Copyright © 2022 All rights reserved.
  * All modification must get authorization from the author.
