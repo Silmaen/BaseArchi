207209f5ff9f6cb58faf7b41045ce482ea19b9f0
Silmaen <genteur.slayer@laposte.net>
2022-07-15 21:17:22 +0200

    Intermediate commit: adding bq24125 battery controler

--- lib/sbs/sensor/Bq24125l.cpp
+++ lib/sbs/sensor/Bq24125l.cpp
@@ -9,6 +9,7 @@
 #include "Bq24125l.h"
 #include "io/i2c/utils.h"
 #include "math/base.h"
+#include "time/timing.h"
 
 #ifdef ARDUINO
 #include <Arduino.h>
@@ -48,21 +49,132 @@ bool Bq24125l::checkPresence() const {
     return getVersion() == chipId;
 }
 
+
+void Bq24125l::setChargingMode(const ChargingMode& mode) {
+    if (!presence()) return;
+    switch (mode) {
+    case ChargingMode::Disconnected:
+        disableBATFET();
+        break;
+    case ChargingMode::Disable: {
+        uint8_t mask = readPowerONRegister() & 0xCF;
+        io::i2c::writeCommand(getAddress(), Registers::POWERON_CONFIG, mask);
+        mask = readChargeTermRegister() & 0x7F;
+        io::i2c::writeCommand(getAddress(), Registers::CHARGE_TIMER_CONTROL, mask);
+        mask = readOpControlRegister() & 0xFC;
+        io::i2c::writeCommand(getAddress(), Registers::MISC_CONTROL, mask);
+        enableBATFET();
+    } break;
+    case ChargingMode::Normal: {
+#ifdef ARDUINO_ARCH_SAMD
+        digitalWrite(PIN_USB_HOST_ENABLE, LOW);
+#endif
+        uint8_t mask = readPowerONRegister() & 0xCF;
+        // Enable PMIC Charge Mode
+        io::i2c::writeCommand(getAddress(), Registers::POWERON_CONFIG, mask | 0x10);
+        mask = readChargeTermRegister() & 0x7F;
+        // Enable Charge Termination Pin
+        io::i2c::writeCommand(getAddress(), Registers::CHARGE_TIMER_CONTROL, mask | 0x80);
+
+        // Enable Charge and Battery Fault Interrupt
+        mask = readOpControlRegister() & 0xFC;
+        io::i2c::writeCommand(getAddress(), Registers::MISC_CONTROL, mask | 0x03);
+        enableBATFET();
+    } break;
+    case ChargingMode::Boost: {
+        uint8_t mask = readPowerONRegister() & 0xCF;
+        // Enable PMIC Boost Mode
+        io::i2c::writeCommand(getAddress(), Registers::POWERON_CONFIG, mask | 0x20);
+#ifdef ARDUINO_ARCH_SAMD
+        digitalWrite(PIN_USB_HOST_ENABLE, LOW);
+#endif
+        // Disable Charge Termination Pin
+        mask = readChargeTermRegister() & 0x7F;
+        io::i2c::writeCommand(getAddress(), Registers::CHARGE_TIMER_CONTROL, mask);
+
+        // Enable Battery Fault interrupt and disable Charge Fault Interrupt
+        mask = readOpControlRegister() & 0xFC;
+        io::i2c::writeCommand(getAddress(), Registers::MISC_CONTROL, mask | 0x03);
+        enableBATFET();
+        // wait for enable boost mode
+        time::delay(500);
+    } break;
+    }
+}
+
+void Bq24125l::enableCharge() {
+    if (!presence()) return;
+#ifdef ARDUINO_ARCH_SAMD
+    digitalWrite(PIN_USB_HOST_ENABLE, LOW);
+#endif
+    uint8_t mask = readPowerONRegister() & 0xCF;
+    // Enable PMIC Charge Mode
+    io::i2c::writeCommand(getAddress(), Registers::POWERON_CONFIG, mask | 0x10);
+    mask = readChargeTermRegister() & 0x7F;
+    // Enable Charge Termination Pin
+    io::i2c::writeCommand(getAddress(), Registers::CHARGE_TIMER_CONTROL, mask | 0x80);
+
+    // Enable Charge and Battery Fault Interrupt
+    mask = readOpControlRegister() & 0xFC;
+    io::i2c::writeCommand(getAddress(), Registers::MISC_CONTROL, mask | 0x03);
+    enableBATFET();
+}
+
+void Bq24125l::enableBoostMode() {
+    if (!presence()) return;
+    uint8_t mask = readPowerONRegister() & 0xCF;
+    // Enable PMIC Boost Mode
+    io::i2c::writeCommand(getAddress(), Registers::POWERON_CONFIG, mask | 0x20);
+#ifdef ARDUINO_ARCH_SAMD
+    digitalWrite(PIN_USB_HOST_ENABLE, LOW);
+#endif
+    // Disable Charge Termination Pin
+    mask = readChargeTermRegister() & 0x7F;
+    io::i2c::writeCommand(getAddress(), Registers::CHARGE_TIMER_CONTROL, mask);
+
+    // Enable Battery Fault interrupt and disable Charge Fault Interrupt
+    mask = readOpControlRegister() & 0xFC;
+    io::i2c::writeCommand(getAddress(), Registers::MISC_CONTROL, mask | 0x03);
+    // wait for enable boost mode
+    time::delay(500);
+}
+
+void Bq24125l::disableCharge() {
+    if (!presence()) return;
+    uint8_t mask = readPowerONRegister() & 0xCF;
+    io::i2c::writeCommand(getAddress(), Registers::POWERON_CONFIG, mask);
+    mask = readChargeTermRegister() & 0x7F;
+    io::i2c::writeCommand(getAddress(), Registers::CHARGE_TIMER_CONTROL, mask);
+    mask = readOpControlRegister() & 0xFC;
+    io::i2c::writeCommand(getAddress(), Registers::MISC_CONTROL, mask);
+}
+
+void Bq24125l::disableBoostMode() {
+    if (!presence()) return;
+#ifdef ARDUINO_ARCH_SAMD
+    digitalWrite(PIN_USB_HOST_ENABLE, HIGH);
+#endif
+    uint8_t mask = readPowerONRegister() & 0xCF;
+    // set default Charger Configuration value
+    io::i2c::writeCommand(getAddress(), Registers::POWERON_CONFIG, mask | 0x10);
+}
+
+
 void Bq24125l::setChargeCurrent(float current) {
     if (!presence()) return;
-    byte mask = io::i2c::read8(getAddress(), Registers::CHARGE_CURRENT_CONTROL) & 0x01;
-    io::i2c::writeCommand(getAddress(), Registers::CHARGE_CURRENT_CONTROL, (round(((math::clamp(current,0.512F,4.544F) - 0.512F) / 0.016F)) & 0xFC) | mask);
+    uint8_t mask = io::i2c::read8(getAddress(), Registers::CHARGE_CURRENT_CONTROL) & 0x01;
+    io::i2c::writeCommand(getAddress(), Registers::CHARGE_CURRENT_CONTROL, (round((math::clamp(current, 0.512F, 4.544F) - 0.512F) / 0.016F) & 0xFC) | mask);
 }
+
 float Bq24125l::getChargeCurrent() const {
     if (!presence()) return 0;
     return ((io::i2c::read8(getAddress(), Registers::CHARGE_CURRENT_CONTROL) & 0xFC) * 0.016F) + 0.512F;
 }
 
-
 void Bq24125l::setPreChargeCurrent(float current) {
     if (!presence()) return;
-    byte mask = io::i2c::read8(getAddress(), Registers::CHARGE_CURRENT_CONTROL) & 0x0F;
-    io::i2c::writeCommand(getAddress(), Registers::PRECHARGE_CURRENT_CONTROL, mask | (round((math::clamp(current,0.128F,2.048F) - 0.128F) / 0.008F) & 0xF0));
+    uint8_t mask = io::i2c::read8(getAddress(), Registers::CHARGE_CURRENT_CONTROL) & 0x0F;
+    io::i2c::writeCommand(getAddress(), Registers::PRECHARGE_CURRENT_CONTROL, mask | (round((math::clamp(current, 0.128F, 2.048F) - 0.128F) / 0.008F) & 0xF0));
 }
 
 [[nodiscard]] float Bq24125l::getPreChargeCurrent() const {
@@ -72,8 +184,8 @@ void Bq24125l::setPreChargeCurrent(float current) {
 
 void Bq24125l::setTermChargeCurrent(float current) {
     if (!presence()) return;
-    byte mask = io::i2c::read8(getAddress(), Registers::CHARGE_CURRENT_CONTROL) & 0xF0;
-    io::i2c::writeCommand(getAddress(), Registers::PRECHARGE_CURRENT_CONTROL, mask | (round((math::clamp(current,0.128F,2.048F) - 0.128F) / 0.128F) & 0x0F));
+    uint8_t mask = io::i2c::read8(getAddress(), Registers::CHARGE_CURRENT_CONTROL) & 0xF0;
+    io::i2c::writeCommand(getAddress(), Registers::PRECHARGE_CURRENT_CONTROL, mask | (round((math::clamp(current, 0.128F, 2.048F) - 0.128F) / 0.128F) & 0x0F));
 }
 
 [[nodiscard]] float Bq24125l::getTermChargeCurrent() const {
@@ -81,11 +193,6 @@ void Bq24125l::setTermChargeCurrent(float current) {
     return ((io::i2c::read8(getAddress(), Registers::CHARGE_CURRENT_CONTROL) & 0x0F) * 0.128F) + 0.128F;
 }
 
-
-bool Bq24125l::isBatteryInOverVoltage() const {
-    if (!presence()) return false;
-    return (readFaultRegister() & 0x08) != 0;
-}
 
 uint8_t Bq24125l::getVersion() const {
     return io::i2c::read8(getAddress(), Registers::PMIC_VERSION);
@@ -98,15 +205,16 @@ bool Bq24125l::isBattConnected() const {
 
 int Bq24125l::USBmode() {
     if (!presence()) return 0xFF;
-    return readSystemStatusRegister() & 0xC0;
+    return readSystemStatusRegister() & 0b11000000;
 }
+
 
 uint8_t Bq24125l::readOpControlRegister() const {
     return io::i2c::read8(getAddress(), Registers::MISC_CONTROL);
 }
 
 uint8_t Bq24125l::readChargeTermRegister() const {
-    return io::i2c::read8(getAddress(), Registers::);
+    return io::i2c::read8(getAddress(), Registers::CHARGE_TIMER_CONTROL);
 }
 
 uint8_t Bq24125l::readPowerONRegister() const {
@@ -119,11 +227,48 @@ uint8_t Bq24125l::readInputSourceRegister() const {
 
 uint8_t Bq24125l::readSystemStatusRegister() const {
     return io::i2c::read8(getAddress(), Registers::SYSTEM_STATUS);
-    ;
 }
+
+// ----------------- FAULT REGISTER MANAGEMENT ------------------------
 
 uint8_t Bq24125l::readFaultRegister() const {
     return io::i2c::read8(getAddress(), Registers::FAULT);
+}
+
+Bq24125l::ThermalFault Bq24125l::getThermalFault() const {
+    auto fault = readFaultRegister();
+    if (fault == 0x00)
+        return ThermalFault::Ok;
+    // Temperature Fault
+    if (fault == 0x05)
+        return ThermalFault::LowerThresholdTemperature;
+    if (fault == 0x06)
+        return ThermalFault::UpperThresholdTemperature;
+    return ThermalFault::Unknown;
+}
+
+Bq24125l::ChargeFault Bq24125l::getChargeFault() const {
+    auto fault = readFaultRegister();
+    if (fault == 0x00)
+        return ChargeFault::Ok;
+    // Charge faults
+    if (fault == 0x10)
+        return ChargeFault::InputOverVoltage;
+    if (fault == 0x20)
+        return ChargeFault::ThermalShutDown;
+    if (fault == 0x30)
+        return ChargeFault::ChargeSafetyTimeExpired;
+    return ChargeFault::Unknown;
+}
+
+bool Bq24125l::isWatchdogExpired() const {
+    if (!presence()) return false;
+    return (readFaultRegister() & 0x80) != 0;
+}
+
+bool Bq24125l::isBatteryInOverVoltage() const {
+    if (!presence()) return false;
+    return (readFaultRegister() & 0x08) != 0;
 }
 
 }// namespace sbs::sensor
--- lib/sbs/sensor/Bq24125l.h
+++ lib/sbs/sensor/Bq24125l.h
@@ -46,22 +46,33 @@ public:
      * @return Return true if the device is found.
      */
     [[nodiscard]] bool checkPresence() const override;
-    bool enableCharge();
-    bool enableBoostMode();
-    bool disableCharge();
-    bool disableBoostMode();
+
+    /**
+     * @brief Power mode
+     */
+    enum struct ChargingMode {
+        Disable,     ///< Charging disconnected
+        Normal,      ///< Charge at normale rate
+        Boost,       ///< Charge in boost rate
+        Disconnected,///< Disconnect battery
+    };
+    void setChargingMode(const ChargingMode& mode);
+    void enableCharge();
+    void enableBoostMode();
+    void disableCharge();
+    void disableBoostMode();
 
     // Input source control register
-    bool enableBuck(void);
-    bool disableBuck(void);
-    bool setInputCurrentLimit(float current);
-    float getInputCurrentLimit(void);
-    bool setInputVoltageLimit(float voltage);
-    float getInputVoltageLimit(void);
+    void enableBuck();
+    void disableBuck();
+    void setInputCurrentLimit(float current);
+    float getInputCurrentLimit();
+    void setInputVoltageLimit(float voltage);
+    float getInputVoltageLimit();
 
     // Power ON configuration register
-    bool resetWatchdog(void);
-    bool setMinimumSystemVoltage(float voltage);
+    void resetWatchdog();
+    void setMinimumSystemVoltage(float voltage);
     float getMinimumSystemVoltage();
     /**
      * @brief Set the charge current.
@@ -72,30 +83,30 @@ public:
      * @brief Get the charge current.
      * @return The charge current
      */
-    [[nodiscard]] float getChargeCurrent()const;
+    [[nodiscard]] float getChargeCurrent() const;
 
     // PreCharge/Termination Current Control Register
     void setPreChargeCurrent(float current);
-    [[nodiscard]] float getPreChargeCurrent()const;
+    [[nodiscard]] float getPreChargeCurrent() const;
     void setTermChargeCurrent(float current);
-    [[nodiscard]] float getTermChargeCurrent()const;
+    [[nodiscard]] float getTermChargeCurrent() const;
 
     // Charge Voltage Control Register
-    bool setChargeVoltage(float voltage);
+    void setChargeVoltage(float voltage);
     float getChargeVoltage();
 
     // Charge Timer Control Register
     void disableWatchdog();
 
     // Misc Operation Control Register
-    bool enableDPDM(void);
-    bool disableDPDM(void);
-    bool enableBATFET(void);
-    bool disableBATFET(void);
-    bool enableChargeFaultINT();
-    bool disableChargeFaultINT();
-    bool enableBatFaultINT();
-    bool disableBatFaultINT();
+    void enableDPDM();
+    void disableDPDM();
+    void enableBATFET();
+    void disableBATFET();
+    void enableChargeFaultINT();
+    void disableChargeFaultINT();
+    void enableBatFaultINT();
+    void disableBatFaultINT();
 
     // System Status Register
     int USBmode();
@@ -110,41 +121,106 @@ public:
     bool canRunOnBattery();
 
     // Fault Register
-    bool isWatchdogExpired();
-    int getChargeFault();
+    enum struct ThermalFault {
+        Ok,
+        LowerThresholdTemperature,
+        UpperThresholdTemperature,
+        Unknown
+    };
+    enum struct ChargeFault {
+        Ok,
+        InputOverVoltage,
+        ThermalShutDown,
+        ChargeSafetyTimeExpired,
+        Unknown
+    };
+    [[nodiscard]] ThermalFault getThermalFault() const;
+    [[nodiscard]] ChargeFault getChargeFault() const;
+
+    [[nodiscard]] bool isWatchdogExpired()const;
     /**
      * @brief If battery over-voltage fault occurs
      * @return True if battery over-voltage
      */
     [[nodiscard]] bool isBatteryInOverVoltage() const;
-    int hasBatteryTemperatureFault();
 
 private:
     enum Registers {
-        INPUT_SOURCE              = 0x00,
-        POWERON_CONFIG            = 0x01,
-        CHARGE_CURRENT_CONTROL    = 0x02,
+        INPUT_SOURCE = 0x00,
+        /**
+         * @brief Power On register
+         *  bits 7   6   5   4   3   2   1   0
+         *              Charge
+         *
+         *  Charge, 00: disable, 01: charge, 10: boost
+         */
+        POWERON_CONFIG = 0x01,
+        /**
+         * @brief Control charge current
+         *  bits 7   6   5   4   3   2   1   0
+         *        -- charge current --
+         */
+        CHARGE_CURRENT_CONTROL = 0x02,
+        /**
+         * @brief Control charge current
+         *  bits 7   6   5   4   3   2   1   0
+         *       -- chg cur --  - th chg cur -
+         */
         PRECHARGE_CURRENT_CONTROL = 0x03,
         CHARGE_VOLTAGE_CONTROL    = 0x04,
-        CHARGE_TIMER_CONTROL      = 0x05,
-        THERMAL_REG_CONTROL       = 0x06,
-        MISC_CONTROL              = 0x07,
-        SYSTEM_STATUS             = 0x08,
-        FAULT                     = 0x09,
-        PMIC_VERSION              = 0x0A,
+        /**
+         * @brief Charge Time control
+         *  bits 7   6   5   4   3   2   1   0
+         *      chg
+         *
+         * chg, 0: disable charge terminason pin, 1: enable
+         */
+        CHARGE_TIMER_CONTROL = 0x05,
+        THERMAL_REG_CONTROL  = 0x06,
+        /**
+         * @brief Miscelaneous configuration
+         *  bits 7   6   5   4   3   2   1   0
+         *                             b_fault_int
+         *
+         * b_fault_int: 11: enable Enable Battery Fault interrupt & Charge Fault Interrupt, 00:disable all:
+         */
+        MISC_CONTROL = 0x07,
+        /**
+         * @brief System status
+         *  bits 7   6   5   4   3   2   1   0
+         *                      BAT
+         *
+         * BAT: Battery is present
+         */
+        SYSTEM_STATUS = 0x08,
+        /**
+         * @brief Fault register
+         *  bits 7   6   5   4   3   2   1   0
+         *       WE     ChgFlt  IOV ThermalFault
+         *
+         * WE          : Watchdog expired
+         * ChgFlt      : Charge fault 01 input over voltage, 10: Thermal shutdown, 11: Charge safety time expired
+         * IOV         : Battery input over voltage
+         * ThermalFault: 101 lowerthreshold temperature, 110 upper threshold temperature
+         */
+        FAULT = 0x09,
+        /**
+         * @brief Version number
+         */
+        PMIC_VERSION = 0x0A,
     };
     // Misc Operation Control Register
-    bool setWatchdog(uint8_t time);
+    void setWatchdog(uint8_t time);
 
     // Thermal Regulation Control Register
-    bool setThermalRegulationTemperature(int degree);
+    void setThermalRegulationTemperature(int degree);
     int getThermalRegulationTemperature();
 
     // Power ON configuration register
-    bool enableCharging(void);
-    bool disableCharging(void);
-    bool enableOTG(void);
-    bool disableOTG(void);
+    void enableCharging();
+    void disableCharging();
+    void enableOTG();
+    void disableOTG();
 
     /**
      * @brief Read the version register
